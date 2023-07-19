5a7eb1efe4dac7c463acec97723b1f64e65bebea
oTales <talesirineu@gmail.com>
2023-07-19 20:05:18 -0300

    feat: subindo alteracao importante

--- lista/eventos.php
+++ lista/eventos.php
@@ -8,130 +8,12 @@ include_once('./config/conexao.php');
 include_once('./func/func.php');
 ?>
 
-<head>
-
-    <section class="intro">
-        <div class="bg-image h-100">
-            <div class="mask d-flex align-items-center h-100">
-                <div class="container">
-                    <div class="row justify-content-center">
-                        <div class="col-12">
-                            <div class="card shadow-2-strong" style="background-color: #f5f7fa;">
-                                <div class="card-body">
-                                    <div class="table-responsive">
-                                        <table class="table table-borderless mb-0">
-                                            <thead>
-                                                <tr>
-                                                    <th scope="col">ID</th>
-                                                    <th scope="col">Imagem</th>
-                                                    <th scope="col">Nome</th>
-                                                    <th scope="col">Valor</th>
-                                                    <th scope="col">Descriçao</th>
-                                                    <th scope="col">Cadastro</th>
-                                                    <th scope="col">Status</th>
-                                                    <th scope="col">Ações</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody class="divide-y divide-gray-100 border-t border-gray-100">
-                                                <?php
-
-                                                $listarEventos = viewAll('*', 'eventos');
-
-                                                foreach ($listarEventos as $listarEventosItem) {
-                                                    $ideventos = $listarEventosItem->ideventos;
-                                                    $img = $listarEventosItem->img;
-                                                    $nome = $listarEventosItem->nome;
-                                                    $valor = $listarEventosItem->valor;
-                                                    $descricao = $listarEventosItem->descricao;
-                                                    $cadastro = $listarEventosItem->cadastro;
-                                                    $ativo = $listarEventosItem->ativo;
-                                                    ?>
-
-                                                    <tr>
-                                                        <th class="form-check">
-                                                                <div>
-                                                                    <?php echo $ideventos ?>
-                                                                </div>
-                                                            
-                                                        </th>
-                                                        <td >
-                                                            <?php echo $img ?>
-                                                        </td>
-                                                        <td >
-                                                            <?php echo $nome ?>
-                                                        </td>
-                                                        <td>
-                                                            <?php echo $valor ?>
-                                                        </td>
-                                                            <?php echo $descricao ?>
-                                                        </td>
-                                                        <td >
-                                                            <?php echo $cadastro ?>
-                                                        </td>
-                                                        <td>
-                                                        
-                                                                Active
-
-
-            
-                                                        </td>
-                                                        <td>
-                                                            <div>
-                                                                
-                                                                <form action="./formsDelete/delete.php" method="post">
-                                                                    <input name="tabela" id="id" value="eventos" hidden
-                                                                        required>
-                                                                    <input name="id" id="id"
-                                                                        value="<?php echo $ideventos; ?>" hidden required>
-                                                                    <button class="fas fa-trash"type="submit">
-                                                                                
-                                                                    </button>
-                                                                </form>
-                                                                <aclas class="fa-regular fa-pen-to-square">
-                                                                
-                                                </aclass>
-                                                            </div>
-                                                        </td>
-                                                    </tr>
-                                                    </tr>
-                                                    <?php
-                                                }
-                                                ?>
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                            </div>
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/lista.css">
-    <script src="./assets/js/modal.js"></script>
-
-    <title>Document</title>
-</head>
-
 <body>
-    <?php
-    include_once('./config/constantes.php');
-    include_once('./config/conexao.php');
-    include_once('./func/func.php');
-    ?>
-
-
-
     <div class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
                     <h1 class="m-0">Eventos</h1>
-                </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Eventos</li>
-                    </ol>
                 </div><!-- /.col -->
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
@@ -147,7 +29,8 @@ include_once('./func/func.php');
         </button>
 
         <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+            aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
@@ -170,6 +53,95 @@ include_once('./func/func.php');
             </div>
         </div>
 
-    </section>
+        </section>
 
+        <head>
 
+            <section class="intro">
+                <div class="bg-image h-100">
+                    <div class="mask d-flex align-items-center h-100">
+                        <div class="container">
+                            <div class="row justify-content-center">
+                                <div class="col-12">
+                                    <div class="card shadow-2-strong" style="background-color: #f5f7fa;">
+                                        <div class="card-body">
+                                            <div class="table-responsive">
+                                                <table class="table table-borderless mb-0">
+                                                    <thead>
+                                                        <tr>
+                                                            <th scope="col">ID</th>
+                                                            <th scope="col">Imagem</th>
+                                                            <th scope="col">Nome</th>
+                                                            <th scope="col">Valor</th>
+                                                            <th scope="col">Descriçao</th>
+                                                            <th scope="col">Cadastro</th>
+                                                            <th scope="col">Status</th>
+                                                            <th scope="col">Ações</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody class="divide-y divide-gray-100 border-t border-gray-100">
+                                                        <?php
+
+                                                        $listarEventos = viewAll('*', 'eventos');
+                                                        foreach ($listarEventos as $listarEventosItem) {
+                                                            $ideventos = $listarEventosItem->ideventos;
+                                                            $img = $listarEventosItem->img;
+                                                            $nome = $listarEventosItem->nome;
+                                                            $valor = $listarEventosItem->valor;
+                                                            $descricao = $listarEventosItem->descricao;
+                                                            $cadastro = $listarEventosItem->cadastro;
+                                                            $ativo = $listarEventosItem->ativo;
+                                                            ?>
+
+                                                            <tr>
+                                                                <td>                                                            
+                                                                        <?php echo $ideventos ?>
+                                                                </td>
+                                                                <td>
+                                                                    <?php echo $img ?>
+                                                                </td>
+                                                                <td>
+                                                                    <?php echo $nome ?>
+                                                                </td>
+                                                                <td>
+                                                                    <?php echo $valor ?>
+                                                                </td>
+                                                                <td>
+                                                                    <?php echo $descricao ?>
+                                                                </td>
+                                                                </td>
+                                                                <td>
+                                                                    <?php echo $cadastro ?>
+                                                                </td>
+                                                                <td>
+                                                                    Active
+                                                                </td>
+                                                                <td>
+                                                                    <div>
+
+                                                                        <form action="./formsDelete/delete.php"
+                                                                            method="post">
+                                                                            <input name="tabela" id="id" value="eventos"
+                                                                                hidden required>
+                                                                            <input name="id" id="id"
+                                                                                value="<?php echo $ideventos; ?>" hidden
+                                                                                required>
+                                                                            <button class="fas fa-trash" type="submit">
+
+                                                                            </button>
+                                                                        </form>
+                                                                        <aclas class="fa-regular fa-pen-to-square">
+
+                                                                            </aclass>
+                                                                    </div>
+                                                                </td>
+                                                            </tr>
+                                                            </tr>
+                                                            <?php
+                                                        }
+                                                        ?>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
